181cd3e94ec7c02cbddd648f90492484d52959b0
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-08-22 00:01:26 +0700

    Add images

--- src/app/data/experience_images.ts
+++ src/app/data/experience_images.ts
@@ -8,6 +8,8 @@ const experience_images: ExperienceImage[] = [
     {src: "/images/experience/oshin_landuage_meetup.png", alt: "Oshin at language meetup"},
     {src: "/images/experience/oshin_at_dm1.png", alt: "Oshin at DevMorning (Songkran 2025) "},
     {src: "/images/experience/oshin_eng_korean.png", alt: "Oshin at 'I wanna be Thai meetup'"},
+    {src: "/images/experience/oshin_and_dev_friends.png", alt: "Oshin and Dev Friends"},
+    {src: "/images/experience/oshin_at_pasa2.png", alt: "Oshin at Pasa meetup"},
 
 ];
 
