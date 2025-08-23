ff7de006e8a66204e85f1e460fd5dc6fd0f3d5a3
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-08-24 02:08:51 +0700

    Adjust image position

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -4,24 +4,39 @@ import { useNavbar } from "../contexts/NavbarProvider";
 function ProjectCard(props: Project) {
   const { title, description, images, githubUrl, techStack, type } = props;
   const { darkMode } = useNavbar();
+  const imgCommonCSS = "w-full max-w-[340px] h-auto rounded-lg shadow-lg";
 
   return (
     <section className="flex items-center max-w-[1000px] h-[66vh]">
-      <div className="w-1/2 flex-col flex">
-        {images.map((image) => {
-          return (
-            <img
-              src={image}
-              alt={title}
-              className="w-full max-w-[360px] h-auto rounded-lg shadow-lg"
-            />
-          );
-        })}
+      <div className={`w-1/2 flex-col flex ${images.length > 1 && "relative"}`}>
+        <img
+          className={`${imgCommonCSS} ${
+            images.length > 1 && "absolute right-64 z-30"
+          }`}
+          src={images[0]}
+          alt={title}
+        />
+        {images[1] && (
+          <img
+            className={`${imgCommonCSS}  absolute top-[-16vh] left-32  z-20`}
+            src={images[1]}
+            alt={title}
+          />
+        )}
+        {images[2] && (
+          <img
+            className={`${imgCommonCSS} absolute top-[-26vh] right-44`}
+            src={images[2]}
+            alt={title}
+          />
+        )}
       </div>
       <div className="w-1/2 text-center">
         <h1 className="text-[36px] font-bold">{title}</h1>
         <span>-{type}- </span>
-        <p className="text-center text-[22px] mt-10 mb-16">{description}</p>
+        <p className="text-center text-[22px] mt-10 mb-16 px-10">
+          {description}
+        </p>
         <p className=" text-[18px] mb-10">Tech Stack: {techStack}</p>
         <div className="w-full">
           <img
