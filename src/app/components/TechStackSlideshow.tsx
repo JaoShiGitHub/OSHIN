a96fe018c5ca1d09976116c9547046353c06b3b2
JaoShiGitHub <ganjanapas.oshin@gmail.com>
2025-09-19 23:51:58 +0700

    Adjust layout

--- src/app/components/TechStackSlideshow.tsx
+++ src/app/components/TechStackSlideshow.tsx
@@ -5,12 +5,12 @@ function TechStackSlideshow() {
   const [currentType, setCurrentType] = useState(0);
 
   return (
-    <section className="sm:hidden flex flex-col items-center gap-y-10">
+    <section className="sm:hidden h-[60vh] flex flex-col items-center gap-y-10">
       <span>{techStackByType[currentType].type}</span>
-      <div className="max-w-[80%] flex flex-wrap justify-center items-center gap-6 ">
+      <div className="max-w-[80%] h-[30vh] flex flex-wrap justify-center items-center gap-6 ">
         {techStackByType[currentType].techStack.map((image, index) => {
           return (
-            <div key={index} className="flex justify-center content-center">
+            <div key={index} className="flex justify-center items-center">
               <img alt={image.alt} src={image.src} className=" max-h-[50px]" />
             </div>
           );
